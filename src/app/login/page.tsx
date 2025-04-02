2c89c2fac6ccbefd22482ca87da5e0a3810e180d
Eduardo RS <erendon@arkusnexus.com>
2025-04-02 09:37:11 -0700

    refactor frontend logic to backend

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,23 @@
-// pages/login.tsx
-const microsoftLoginUrl = `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_AZURE_TENANT_ID}/oauth2/v2.0/authorize?client_id=${process.env.NEXT_PUBLIC_AZURE_CLIENT_ID}&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_AZURE_REDIRECT_URI}&response_mode=query&scope=openid profile email`;
+// // pages/login.tsx
+// const microsoftLoginUrl = `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_AZURE_TENANT_ID}/oauth2/v2.0/authorize?client_id=${process.env.NEXT_PUBLIC_AZURE_CLIENT_ID}&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_AZURE_REDIRECT_URI}&response_mode=query&scope=openid profile email`;
+
+// export default function Login() {
+//   return (
+//     <div>
+//       <h1>Login</h1>
+//       <a href={microsoftLoginUrl}>
+//         <button>Microsoft Login</button>
+//       </a>
+//     </div>
+//   );
+// }
 
 export default function Login() {
   return (
     <div>
-      <h1>Login</h1>
-      <a href={microsoftLoginUrl}>
-        <button>Microsoft Login</button>
+      <h1>Inicia sesión</h1>
+      <a href="http://localhost:3000/auth/microsoft/login">
+        <button>Iniciar sesión con Microsoft</button>
       </a>
     </div>
   );
